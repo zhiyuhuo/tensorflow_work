e74743fa732ede50094d73a11646d7b4d2377aef
zhiyuhuo <huozhiyunewneu@gmail.com>
2016-11-08 15:41:11 -0600

    chunk and words feature together

---
+++ rnn_spatial_language_parsing/data_input_2.py
@@ -0,0 +1,173 @@
+import numpy as np
+import tensorflow as tf
+
+def read_array_from_string(cstr):
+    res = [float(s) for s in cstr.split('  ')]
+    return res
+
+def read_a_data_file(filename):
+    with open(filename) as f:
+        contents = f.readlines()
+        
+    vec_chunk = []
+    mat_words = []
+    vec_rm = []   
+    vec_obj = []
+    vec_ref = []
+    vec_dir = []
+    vec_tar = []
+        
+    # get chunk name data
+    i = 0
+    cstr = contents[i]
+    cstr = cstr[0:len(cstr)-1]
+    while cstr != "inchunk":
+        i = i + 1
+        cstr = contents[i]
+        cstr = cstr[0:len(cstr)-1]
+        
+    i = i + 1
+    cstr = contents[i]
+    cstr = cstr[0:len(cstr)-1]
+    vec_chunk = read_array_from_string(cstr);
+	    
+    # get word feature data
+    while cstr != "inwords":
+        i = i + 1
+        cstr = contents[i]
+        cstr = cstr[0:len(cstr)-1]
+        
+    i = i + 1
+    cstr = contents[i]
+    cstr = cstr[0:len(cstr)-1]    
+    while cstr != "outroom":
+        mat_words.append(read_array_from_string(cstr))
+        i = i + 1
+        cstr = contents[i]
+        cstr = cstr[0:len(cstr)-1]
+    
+    # get room label data
+    i = i + 1
+    cstr = contents[i]
+    cstr = cstr[0:len(cstr)-1]
+    vec_rm = read_array_from_string(cstr);
+    
+    #get obj label data
+    i = i + 1
+    cstr = contents[i]
+    cstr = cstr[0:len(cstr)-1]   
+    while cstr != "outobj":
+        i = i + 1
+        cstr = contents[i]
+        cstr = cstr[0:len(cstr)-1]
+        
+    i = i + 1
+    cstr = contents[i]
+    cstr = cstr[0:len(cstr)-1]
+    vec_obj = read_array_from_string(cstr);
+    
+    # get ref label data
+    i = i + 1
+    cstr = contents[i]
+    cstr = cstr[0:len(cstr)-1]
+    while cstr != "outref":
+        i = i + 1
+        cstr = contents[i]
+        cstr = cstr[0:len(cstr)-1]
+        
+    i = i + 1
+    cstr = contents[i]
+    cstr = cstr[0:len(cstr)-1]
+    vec_ref = read_array_from_string(cstr);
+    
+    # get dir label data
+    i = i + 1
+    cstr = contents[i]
+    cstr = cstr[0:len(cstr)-1]
+    while cstr != "outdir":
+        i = i + 1
+        cstr = contents[i]
+        cstr = cstr[0:len(cstr)-1]
+        
+    i = i + 1
+    cstr = contents[i]
+    cstr = cstr[0:len(cstr)-1]
+    vec_dir = read_array_from_string(cstr);
+    
+    # get tar label data
+    i = i + 1
+    cstr = contents[i]
+    cstr = cstr[0:len(cstr)-1]
+    while cstr != "outtar":
+        i = i + 1
+        cstr = contents[i]
+        cstr = cstr[0:len(cstr)-1]
+        
+    i = i + 1
+    cstr = contents[i]
+    cstr = cstr[0:len(cstr)-1]
+    vec_tar = read_array_from_string(cstr);
+	    
+    return vec_chunk, mat_words, vec_rm, vec_obj, vec_ref, vec_dir, vec_tar
+  
+def get_lm_input_data(N):
+    N_max_seq_len = 10
+    
+    chunk_in = []
+    words_in = []
+    len_words = []
+    rm_out = []
+    obj_out = []
+    ref_out = []
+    dir_out = []
+    tar_out = []
+  
+    for i in range(1,N+1):
+        filename = "vec/" + str(i) + ".txt"
+        vec_chunk, mat_words, vec_rm, vec_obj, vec_ref, vec_dir, vec_tar = read_a_data_file(filename)
+        #print(vec_chunk)
+        #print(mat_words)
+        #print(vec_rm)
+        #print(vec_obj)
+        #print(vec_ref)
+        #print(vec_dir)
+        #print(vec_tar)
+        
+        # the input chunk feature 
+        chunk_in.append(vec_chunk)
+        
+        # the input words feature
+        if len(mat_words) < N_max_seq_len:
+	    D_words = len(mat_words[0])
+	    len_words.append(len(mat_words))
+	    
+	    for k in range(len(mat_words), N_max_seq_len):
+	        mat_words.append([0.0]*D_words)
+	    words_in.append(mat_words)
+	else:
+	    D_words = N_max_seq_len
+	    mat_words = mat_words[0:N_max_seq_len]
+	    words_in.append(mat_words)
+	    
+	# the output label of room
+	rm_out.append(vec_rm)
+	
+	# the output label of object
+	obj_out.append(vec_obj)
+	
+	# the output label of reference
+	ref_out.append(vec_ref)
+	
+	# the output label of direction
+	dir_out.append(vec_dir)
+	
+	# the output label of target
+	tar_out.append(vec_tar)
+        
+    return chunk_in, words_in, rm_out, obj_out, ref_out, dir_out, tar_out, len_words
+ 
+
+if __name__ == "__main__":
+    chunk_in, words_in, rm_out, obj_out, ref_out, dir_out, tar_out, len_words = get_lm_input_data(818)
+    
+    
---
+++ rnn_spatial_language_parsing/drnn_spl_room.py
@@ -0,0 +1,230 @@
+'''
+A Dynamic Recurrent Neural Network (LSTM) implementation example using
+TensorFlow library. This program will use drnn on the spatial language data to build a classifier on spatial clauses
+
+Long Short Term Memory paper: http://deeplearning.cs.cmu.edu/pdfs/Hochreiter97_lstm.pdf
+
+Author: Aymeric Damien
+Project: https://github.com/aymericdamien/TensorFlow-Examples/
+'''
+
+from __future__ import print_function
+
+import tensorflow as tf
+import random
+from data_input_2 import get_lm_input_data 
+
+
+# ====================
+#  import binary language feature data
+# ====================
+class LanguageSequenceData(object):
+    """ Generate sequence of data with dynamic length.
+    This class generate samples for training:
+
+    NOTICE:
+    We have to pad each sequence to reach 'max_seq_len' for TensorFlow
+    consistency (we cannot feed a numpy array with inconsistent
+    dimensions). The dynamic calculation will then be perform thanks to
+    'seqlen' attribute that records every actual sequence length.
+    """
+    """
+    NOTICE:
+    We have to pad each sequence to reach 'max_seq_len' for TensorFlow
+    consistency (we cannot feed a numpy array with inconsistent
+    dimensions). The dynamic calculation will then be perform thanks to
+    'seqlen' attribute that records every actual sequence length.
+    """
+    
+    def __init__(self, max_seq_len=10):
+        self.chunk = []
+        self.words = []
+        self.labels = []
+        self.seqlen = []
+        n_samples = 818
+        chunk_in, words_in, rm_out, obj_out, ref_out, dir_out, tar_out, len_words = get_lm_input_data(n_samples)
+        print(len(chunk_in))
+        print(len(words_in))
+        print(len(len_words))
+        print(len(rm_out))
+        
+        print(chunk_in[1])
+        print(words_in[1])
+        print(len_words[1])
+        print(rm_out[1])
+        
+        for n in range(n_samples):
+	    self.chunk.append(chunk_in[n])
+            self.words.append(words_in[n])
+            self.labels.append(rm_out[n])
+            self.seqlen.append(len_words[n])
+        self.batch_id = 0
+
+    def next(self, batch_size):
+        """ Return a batch of data. When dataset end is reached, start over.
+        """
+        L = len(self.words)
+        if self.batch_id == L:
+            self.batch_id = 0
+        batch_chunk = (self.chunk[self.batch_id:min(self.batch_id +
+                                                  batch_size, L)])
+        batch_words = (self.words[self.batch_id:min(self.batch_id +
+                                                  batch_size, L)])
+        batch_labels = (self.labels[self.batch_id:min(self.batch_id +
+                                                  batch_size, L)])
+        batch_seqlen = (self.seqlen[self.batch_id:min(self.batch_id +
+                                                  batch_size, L)])
+        self.batch_id = min(self.batch_id + batch_size, L)
+        return batch_chunk, batch_words, batch_labels, batch_seqlen
+
+
+# ==========
+#   MODEL
+# ==========
+
+# Parameters
+learning_rate = 0.001
+training_iters = 50000
+batch_size = 16
+display_step = 10
+
+# Network Parameters
+n_chunk = 7
+n_words = 66
+seq_max_len = 10 # Sequence max length
+n_hidden = 128 # hidden layer num of features
+n_classes = 2 # linear sequence or not
+
+trainset = LanguageSequenceData(seq_max_len)
+testset = LanguageSequenceData(seq_max_len)
+
+# tf Graph input
+x_chunk = tf.placeholder("float", [None, n_chunk])
+x_words = tf.placeholder("float", [None, seq_max_len, n_words])
+y = tf.placeholder("float", [None, n_classes])
+# A placeholder for indicating each sequence length
+seqlen = tf.placeholder(tf.int32, [None])
+
+# Define weights
+weights = {
+    'lstm': tf.Variable(tf.random_normal([n_hidden, n_classes])),
+    'chunk': tf.Variable(tf.random_normal([n_chunk, n_classes])),
+    'out': tf.Variable(tf.random_normal([n_classes + n_classes, n_classes])),
+}
+biases = {
+    'lstm': tf.Variable(tf.random_normal([n_classes])),
+    'chunk': tf.Variable(tf.random_normal([n_classes])),
+    'out': tf.Variable(tf.random_normal([n_classes])),
+}
+
+
+def dynamicRNN(x, seqlen, weights, biases):
+
+    # Prepare data shape to match `rnn` function requirements
+    # Current data input shape: (batch_size, n_steps, n_input)
+    # Required shape: 'n_steps' tensors list of shape (batch_size, n_input)
+
+    # Permuting batch_size and n_steps
+    x = tf.transpose(x, [1, 0, 2])
+    # Reshaping to (n_steps*batch_size, n_input)
+    x = tf.reshape(x, [-1, n_words])
+    # Split to get a list of 'n_steps' tensors of shape (batch_size, n_input)
+    x = tf.split(0, seq_max_len, x)
+
+    # Define a lstm cell with tensorflow
+    lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(n_hidden)
+
+    # Get lstm cell output, providing 'sequence_length' will perform dynamic
+    # calculation.
+    outputs, states = tf.nn.rnn(lstm_cell, x, dtype=tf.float32, sequence_length=seqlen)
+
+    # When performing dynamic calculation, we must retrieve the last
+    # dynamically computed output, i.e., if a sequence length is 10, we need
+    # to retrieve the 10th output.
+    # However TensorFlow doesn't support advanced indexing yet, so we build
+    # a custom op that for each sample in batch size, get its length and
+    # get the corresponding relevant output.
+
+    # 'outputs' is a list of output at every timestep, we pack them in a Tensor
+    # and change back dimension to [batch_size, n_step, n_input]
+    outputs = tf.pack(outputs)
+    outputs = tf.transpose(outputs, [1, 0, 2])
+
+    # Hack to build the indexing and retrieve the right output.
+    batch_size = tf.shape(outputs)[0]
+    # Start indices for each sample
+    index = tf.range(0, batch_size) * seq_max_len + (seqlen - 1)
+    # Indexing
+    outputs = tf.gather(tf.reshape(outputs, [-1, n_hidden]), index)
+
+    # Linear activation, using outputs computed above
+    return tf.matmul(outputs, weights['lstm']) + biases['lstm']
+
+words_pred = dynamicRNN(x_words, seqlen, weights, biases)
+chunks_pred = tf.matmul(x_chunk, weights['chunk']) + biases['chunk']
+
+pred = tf.matmul(tf.concat(1, [words_pred, chunks_pred]), weights['out']) + biases['out']
+
+
+# Define loss and optimizer
+cost = tf.sqrt(tf.reduce_mean(tf.square(tf.sub(pred, y)))) 
+#cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(pred, y))
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(cost)
+
+# Evaluate model
+correct_pred = tf.equal(tf.argmax(pred,1), tf.argmax(y,1))
+accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+
+# Initializing the variables
+init = tf.initialize_all_variables()
+
+def classify(output, target):
+    res = 0
+    if output[0] > 0.5 and output[1] < 0.5:
+        if target[0] > 0.5 and target[1] < 0.5:
+	    res = 1	    
+    if output[1] > 0.5 and output[0] < 0.5:
+        if target[1] > 0.5 and target[0] < 0.5:
+	    res = 1	    
+    if output[0] < 0.5 and output[1] < 0.5:
+        if target[0] < 0.5 and target[1] < 0.5:
+	    res = 1
+    else:
+        res = 0
+    return res
+
+# Launch the graph
+with tf.Session() as sess:
+    sess.run(init)
+    step = 1
+    # Keep training until reach max iterations
+    while step * batch_size < training_iters:
+        batch_chunk, batch_words, batch_y, batch_seqlen = trainset.next(batch_size)
+        
+        #show the data
+        #print(batch_x[0])
+        #print(batch_y[0])
+        #print(batch_seqlen[0])
+        
+        # Run optimization op (backprop)
+        sess.run(optimizer, feed_dict={x_chunk: batch_chunk, x_words: batch_words, y: batch_y, seqlen: batch_seqlen})
+        if step % display_step == 0:
+            # Calculate batch accuracy
+            acc = sess.run(accuracy, feed_dict={x_chunk: batch_chunk, x_words: batch_words, 
+						y: batch_y, seqlen: batch_seqlen})
+            # Calculate batch loss
+            loss = sess.run(cost, feed_dict={x_chunk: batch_chunk, x_words: batch_words, 
+						y: batch_y, seqlen: batch_seqlen})
+            print("Iter " + str(step*batch_size) + ", Minibatch Loss= " + \
+                  "{:.6f}".format(loss) + ", Training Accuracy= " + \
+                  "{:.5f}".format(acc))
+        step += 1
+    print("Optimization Finished!")
+
+    # Calculate accuracy
+    test_data = testset.data
+    test_label = testset.labels
+    test_seqlen = testset.seqlen
+    print("Testing Accuracy:", \
+        sess.run(accuracy, feed_dict={x_chunk: batch_chunk, x_words: batch_words, 
+						y: batch_y, seqlen: batch_seqlen}))
